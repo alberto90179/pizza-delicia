9e55484e278a61f77d2ec7f6df3e67d3742ba8a3
GulliFerWD <clxl22@hotmail.com>
2025-11-02 13:44:27 -0600

    LOGIN PARCIALMENTE FUNCIONANDO
    Se vinculó el login del front end con el servicio de login de laravel, el cual responde con un token que se guarda en local storage, ese token será necesario para acceder a las demas rutas del crud del servidor.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,12 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loginState, setLoginState] = useState(false);
+  const [loginData, setLoginData] = useState({});
 
   useEffect(() => {
     // Verifica si ya hay sesión guardada
@@ -11,20 +14,60 @@ export function AuthProvider({ children }) {
     if (storedUser) setUser(JSON.parse(storedUser));
   }, []);
 
+  const getLogin = (email, password) => {
+    const res = axios
+      .post("https://service-pizzadelicia-v1.gulliferwd.com/api/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        setLoginData(res.data);
+        setLoginState(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const getLogout = async (token) => {
+    try {
+      const res = await axios.get(
+        "https://service-pizzadelicia-v1.gulliferwd.com/api/logout",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res.data);
+      setLoginState(false);
+      setLoginData({});
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const login = (email, password) => {
-    // Simulación de autenticación
-    if (email === "admin@pizza-delicia.com" && password === "admin123") {
-      const fakeUser = { email, name: "Administrador" };
+    getLogin(email, password);
+    // Se comprueba si el servidor manda el estatus verdadero
+    if (loginState === true) {
+      const fakeUser = {
+        email,
+        name: "Administrador",
+        token: loginData.token,
+      };
       setUser(fakeUser);
       localStorage.setItem("user", JSON.stringify(fakeUser));
       return true;
+    } else if (loginState === false) {
+      return false;
     }
-    return false;
   };
 
   const logout = () => {
     setUser(null);
     localStorage.removeItem("user");
+    getLogout(loginData.token);
   };
 
   return (
